e937a811e876dc431f14c6519028089718d910a0
Denis Ngugi Gathondu <thundoss@gmail.com>
2018-10-30 00:07:27 +0300

    Add lodash Refactor stars and numbers component to use lodash range fuction

--- src/components/game.js
+++ src/components/game.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 
 const Stars = (props) => {
+    const numberOfStars = 1 + Math.floor(Math.random() + 9);
+
     return (
         <div className="col-5">
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
+            { _.range(numberOfStars).map(i =>
+                <i key={i} className="fa fa-star"></i>
+            )}
         </div>
     );
 }
@@ -32,13 +34,15 @@ const Numbers = (props) => {
     return (
         <div className="card text-center">
             <div>
-                <span>1</span>
-                <span className="selected">2</span>
-                <span className="used">3</span>
+                {Numbers.list.map((number, i) =>
+                    <span key={i}>{number}</span>
+                )}
             </div>
         </div>
     );
 }
+
+Numbers.list = _.range(1, 10);
 
 
 class Game extends Component {
