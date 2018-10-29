5a6e4b380425c4254e279956e88227eda7da09a5
Denis Ngugi Gathondu <thundoss@gmail.com>
2018-10-29 22:43:42 +0300

    Add Bootstrap

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# react-math-skills
-A maths skill game created in react
---
+++ src/components/game.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+
+const Stars = (props) => {
+    return (
+        <div>
+            ...
+        </div>
+    );
+}
+
+const Button = (props) => {
+    return (
+        <div>
+            ...
+        </div>
+    );
+}
+
+const Answer = (props) => {
+    return (
+        <div>
+            ...
+        </div>
+    );
+}
+
+
+class Game extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Play Nine</h3>
+                <Stars />
+                <Button />
+                <Answer />
+            </div>
+        );
+    }
+}
+
+export default Game
